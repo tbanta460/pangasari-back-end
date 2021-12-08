ab725528d0728ac98e02d53c492b96b5f932252a
tbanta460 <chitandasu@yahoo.com>
2021-12-08 20:19:53 +0700

    Menghapus front-end dari dirname

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(multer({storage:fileImage.fileStorage, filterFile:fileImage.filterFile})
 
 
 if (process.env.NODE_ENV === "production"{
-      app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+      app.use(express.static(path.join(__dirname, 'build')));
       app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
       });
