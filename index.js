ed567c7b44434e3b71251303a29e1e44c7fc5f23
tbanta460 <chitandasu@yahoo.com>
2021-12-10 12:47:26 +0700

    Menambahkan code di line 47-50 di index.js

--- index.js
+++ index.js
@@ -44,7 +44,11 @@ app.use('/user', dashboardRoute);
 
 mongoose.connect(mongoDbUrl, {useNewUrlParser: true})
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
 }
 
 app.listen(isPort, () => console.log('success'))
